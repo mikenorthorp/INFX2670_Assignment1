9e50b4d9c2011d9b0329d51576ac09d11d48481f
Mike Northorp <mike.northorp@gmail.com>
2014-01-17 08:43:34 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HAS ThE INTERNET GONE TOO FAR?</title>
+</head>
+<body>
+<div id="page">
+	<div id="page-inner">
+	<h1>Form Elements</h1>
+
+	<?php
+		# Show errors
+		ini_set('display_errors', 1);
+	?>	
+
+	</div>
+</div>
+
+</body>
+</html>
