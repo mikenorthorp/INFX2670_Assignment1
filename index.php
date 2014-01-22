e13eaf070245117af21be1ad8cc185d22d27d374
Mike Northorp <mike.northorp@gmail.com>
2014-01-22 12:35:01 -0400

    Added basic html elements to page

--- index.php
+++ index.php
@@ -1,18 +1,43 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>HAS ThE INTERNET GONE TOO FAR?</title>
+	<title>Internet is Awesome</title>
 </head>
 <body>
-<div id="page">
-	<div id="page-inner">
-	<h1>Form Elements</h1>
+<div id="content">
+	<h1> INFX 2670 Assignment 1 - Mike Northorp </h1>
+	<div id="form">
+		<form>
+			<h1> Internet is Awesome Survey </h1>
+			<input type="text" name="text_field" size=20> <br>
 
-	<?php
-		# Show errors
-		ini_set('display_errors', 1);
-	?>	
+			<textarea name="text_area" cols=40 rows=8></textarea> <br>
 
+			<input type="radio" name="radio1" value="Milk"> Milk<br>
+			<input type="radio" name="radio1" value="Butter"> Butter<br>
+			<input type="radio" name="radio1" value="Cheese"> Cheese<br>
+			
+			<input type="checkbox" name="option1" value="Milk"> Milk<br>
+			<input type="checkbox" name="option2" value="Butter"> Butter<br>
+			<input type="checkbox" name="option3" value="Cheese"> Cheese<br>
+
+			<select>
+			  <option value="volvo">Volvo</option>
+			  <option value="saab">Saab</option>
+			  <option value="mercedes">Mercedes</option>
+			</select>
+
+			<div id="email_toggle">
+				<input type="checkbox" name="option1" value="send_email"> Send Email<br>
+				<input type="text" name="text_field" size=20> <br>
+			</div>
+
+			<input type="submit" value="Submit">
+		</form>
+	</div>
+
+	<div id="results" style="display:none">
+		<h1>Results</h1>
 	</div>
 </div>
 
---
+++ results.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RESULTS - HAS THE INTERNET GONE TOO FAR?</title>
+</head>
+<body>
+<div id="page">
+	<div id="page-inner">
+	<h1></h1>
+
+	<?php
+		# Show errors
+		ini_set('display_errors', 1);
+	?>	
+
+	</div>
+</div>
+
+</body>
+</html>
